1c29bf9986db2abd619069987f8cb3471867e8d6
rocasfero <rocasfero@gmail.com>
2020-06-20 16:47:46 +0900

    change win key

--- keyboards/lily58/keymaps/roca/keymap.c
+++ keyboards/lily58/keymaps/roca/keymap.c
@@ -36,6 +36,8 @@ enum custom_keycodes {
 #define RC_SBSP SFT_T(KC_BSPC)
 #define RC_SESC SFT_T(KC_ESC)
 #define RC_LANG LCTL(KC_SPC)
+#define RC_GLBR GUI_T(KC_LBRC)
+#define RC_GRBR GUI_T(KC_RBRC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -45,9 +47,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //|-------+-------+-------+-------+-------+-------|                 |-------+-------+-------+-------+-------+-------|
     KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,KC_MINS,\
 //|-------+-------+-------+-------+-------+-------|                 |-------+-------+-------+-------+-------+-------|
-   KC_LGUI,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                     KC_H,   KC_J,   KC_K,   KC_L,KC_SCLN,KC_QUOT,\
+  KC_LCTRL,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                     KC_H,   KC_J,   KC_K,   KC_L,KC_SCLN,KC_QUOT,\
 //|-------+-------+-------+-------+-------+-------+-------| |-------+-------+-------+-------+-------+-------+-------|
-   RC_SESC,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,KC_LBRC,  KC_RBRC,   KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,RC_SBSP,\
+   RC_SESC,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,RC_GLBR,  RC_GRBR,   KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,RC_SBSP,\
 //|-------+-------+-------+-------+-------+-------+-------| |-------+-------+-------+-------+-------+-------+-------|
                            KC_LALT,RC_CEQL,  LOWER,RC_CSPC,  RC_SENT,  RAISE,RC_CMIN,KC_RALT \
 //                        |-------+-------+-------+-------| |-------+-------+-------+-------|
